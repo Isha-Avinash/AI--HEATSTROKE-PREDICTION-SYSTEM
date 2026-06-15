c90bbd48c652b02c678065c797ff1fe1ce3e4b2f
Adarsh kumar singh <saadarshsingh@gmail.com>
2026-06-15 15:20:31 +0530

    Delete voice_alert.py

--- voice_alert.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-
-def speak(text):
-    engine = pyttsx3.init()
-    engine.setProperty('rate', 150)   # speed
-    engine.setProperty('volume', 1)   # volume max
-
-    engine.say(text)
-    engine.runAndWait()
