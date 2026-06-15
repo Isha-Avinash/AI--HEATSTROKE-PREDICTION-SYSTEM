c914fc7ad36822c6ed5d16f0e81526d462a39442
Adarsh kumar singh <saadarshsingh@gmail.com>
2026-06-15 15:07:49 +0530

    Delete weather.py

--- weather.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def get_weather(city):
-    try:
-        url = f"https://wttr.in/{city}?format=3"
-        return requests.get(url).text
-    except:
-        return "Weather data not available"
